62d5b241c8b5b93bfb0a88fb0aebac6f60e41500
Zachary Walden <zachary.walden@eagles.oc.edu>
2025-07-01 01:49:53 -0500

    feat: add logging, fix: improper usage of send(), read()

--- desmume/src/frontend/posix/shared/matrix_client.cpp
+++ desmume/src/frontend/posix/shared/matrix_client.cpp
@@ -1,11 +1,19 @@
 #include "matrix_client.h"
 #include "glib.h"
+#include <cerrno>
+#include <sys/socket.h>
 
 matrix_client::matrix_client()
 {
 }
 matrix_client::matrix_client(std::string addr)
 {
+	//create socket
+	if((this->client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		g_print("Failed to createsocket");
+		this->conn_valid = false;
+	}
 	//from required address info with  passed string.
 	//split string on ':'
 	int colon = addr.find(':');
@@ -28,20 +36,54 @@ matrix_client::matrix_client(std::string addr)
 	if ((status = connect(this->client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0)
 	{
 		g_printerr("Connecting to matrix sink server failed\n");
+		if(errno == EACCES)
+			g_printerr("EACCES");
+		else if(errno == EPERM)
+			g_printerr("EPERM");
+		else if(errno == EADDRINUSE)
+			g_printerr("EADDRINUSE");
+		else if(errno == EADDRNOTAVAIL)
+			g_printerr("EADDRNOTAVAIL");
+		else if(errno == EAFNOSUPPORT)
+			g_printerr("EAFNOSUPPORT");
+		else if(errno == EAGAIN)
+			g_printerr("EAGAIN");
+		else if(errno == EBADF)
+			g_printerr("EBADF");
+		else if(errno == ECONNREFUSED)
+			g_printerr("ECONNREFUSED");
+		else if(errno == EFAULT)
+			g_printerr("EFAULT");
+		else if(errno == EINPROGRESS)
+			g_printerr("EINPROGRESS");
+		else if(errno == EINTR)
+			g_printerr("EINTR");
+		else if(errno == EISCONN)
+			g_printerr("EISCONN");
+		else if(errno == ENETUNREACH)
+			g_printerr("ENETUNREACH");
+		else if(errno == ENOTSOCK)
+			g_printerr("ENOTSOCK");
+		else if(errno == EPROTOTYPE)
+			g_printerr("EPROTOTYPE");
+		else if(errno == ETIMEDOUT)
+			g_printerr("ETIMEDOUT");
+		else
+			g_printerr(strerror(errno));
 		this->conn_valid = false;
     }
 	//send handshake packet
 	HandshakeHeader hndshk;
 	hndshk.success = false;
 	hndshk.req_protocol_vers = PROTOCOL_VERSION;
-	valsend = send(this->client_fd, (u8*)(&hndshk), sizeof(HandshakeHeader), 0);
+	valsend = this->send_all(this->client_fd, (u8*)(&hndshk), sizeof(HandshakeHeader), 0);
 	if(valsend < 0)
 	{
 		g_printerr("First handshake failed send to server\n");
 		this->conn_valid = false;
 	}
 	//read handshake response from server
-	valread = recv(this->client_fd, (u8*)(&hndshk), sizeof(HandshakeHeader),0);
+	valread = this->recv_all(this->client_fd, (u8*)(&hndshk), sizeof(HandshakeHeader));
 	if(valread < 0)
 	{
 		g_printerr("Failed to receive handshake packet from server\n");
@@ -59,7 +101,7 @@ matrix_client::matrix_client(std::string addr)
 		if(hndshk.req_protocol_vers == PROTOCOL_VERSION)
 		{
 			hndshk.success = 1;
-			valsend = send(this->client_fd, (u8*)(&hndshk), sizeof(HandshakeHeader), 0);
+			valsend = this->send_all(this->client_fd, (u8*)(&hndshk), sizeof(HandshakeHeader), 0);
 			if(valsend < 0)
 			{
 				g_printerr("Second handshake failed send to server\n");
@@ -74,6 +116,7 @@ matrix_client::matrix_client(std::string addr)
 			close(this->client_fd);
 		}
 	}
+	g_printerr("Conn established\n");
 }
 matrix_client::~matrix_client()
 {
@@ -95,23 +138,49 @@ bool matrix_client::send_frame(u16 *buffer,int height, int width)
 	frame_header.h_loc = 0;
 	frame_header.v_loc = 0;
 	frame_header.priority = 0xFF;
-	frame_header.protocol_vers = 0x00;
+	frame_header.protocol_vers = PROTOCOL_VERSION;
 	frame_header.intensity = 0.0f;
+	g_printerr("X = %d, Y = %d\n", width, height);
 	int valsend;
-	valsend = send(this->client_fd, (u8*)(&frame_header), sizeof(frame_header), 0);
+	valsend = this->send_all(this->client_fd, (u8*)(&frame_header), sizeof(frame_header), 0);
 	if(valsend != -1)
 		ret_val = true;
 	else
 		g_printerr("Frame header failed to send to the server\n");
-	valsend = send(this->client_fd, (u8*)buffer, height * width * 2, 0);
+	valsend = this->send_all(this->client_fd, (u8*)buffer, height * width * 2, 0);
 	if(valsend != -1)
 		ret_val = true;
 	else
-		g_printerr("Frame failed to send to the server\n");
+		g_printerr("Frame failed to, const void buf[.size], size_t size, int flags send to the server\n");
 	return ret_val;
 }
 
+int matrix_client::send_all(int sock_fd, const void* buf, size_t size, int flags)
+{
+	long bytes_sent = 0;
 
+	do
+	{
+		bytes_sent += send(sock_fd, ((u8* )buf) + bytes_sent, size - bytes_sent, flags);
+		if(bytes_sent == -1)
+			break;
+	}while(bytes_sent < size);
+
+	return bytes_sent;
+}
+
+int matrix_client::recv_all(int sockfd, void *buf, size_t size)
+{
+	long bytes_recvd = 0;
+	do
+	{
+		bytes_recvd += read(sockfd, ((u8*)buf) + bytes_recvd, size - bytes_recvd);
+		if(bytes_recvd == -1)
+			break;
+	}while(bytes_recvd < size);
+
+	return bytes_recvd;
+}
 
 
 
